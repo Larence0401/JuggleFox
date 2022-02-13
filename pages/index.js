25b4dcab2621c1b4c3ac955143456d6529d2daac
unknown <aventuras030@gmail.com>
2022-02-13 14:04:25 +0100

    escaped characters in string

--- pages/index.js
+++ pages/index.js
@@ -30,15 +30,9 @@ user ? console.log(user.uid) : null
         <head>
           <style>
             @import
-            url('https://fonts.googleapis.com/css2?family=Orbitron&amp;display=swap');
-            url('https://fonts.googleapis.com/css2?family=JetBrains+Mono&amp;family=Orbitron&amp;display=swap')
-            <link
-              rel="stylesheet"
-              href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
-              integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA=="
-              crossOrigin="anonymous"
-              referrerpolicy="no-referrer"
-            />
+            url(&lsquo;https://fonts.googleapis.com/css2?family=Orbitron&amp;display=swap&rsquo;);
+            url(&lsquo;https://fonts.googleapis.com/css2?family=JetBrains+Mono&amp;family=Orbitron&amp;display=swap&rsquo;)
+            
           </style>
         </head>
         <div id="container" className="flex bg-blue-300 w-full h-screen">
